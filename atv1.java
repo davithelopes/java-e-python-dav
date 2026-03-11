c0a555f4d8bd8e2dcc49ad9f7ef58717741d1e24
zy <ddavilopess@gmail.com>
2026-03-11 08:49:54 -0300

    Add files via upload

---
+++ atv1.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+public class atv1 {
+    public static void main(String[] args) {
+
+        try (Scanner sc = new Scanner(System.in)) {
+            int n1, n2;
+
+            System.out.print("digite um numero: ");
+            n1 = sc.nextInt();
+
+            System.out.print("digite um segundo numero: ");
+            n2 = sc.nextInt();
+
+            if (n1 > n2) {
+                System.out.println("o maior numero é: " + n1);
+            } else {
+                System.out.println("o maior numero é: " + n2);
+            }
+        }
+    }
+}
